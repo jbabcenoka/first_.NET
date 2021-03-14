1a6b73124a4fc83f651628a95a859dc8fbcfb926
Jevgenija Babcenoka <57482565+jbabcenoka@users.noreply.github.com>
2021-03-14 22:44:59 +0200

    First

---
+++ PirmaisMajasDarbs/PirmaisMajasDarbs/IOrderManager.cs
@@ -0,0 +1,23 @@
+﻿using Order1;
+using Person1;
+using Product1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PirmaisMajasDarbs
+{
+    public interface IOrderManager 
+    {
+        void AddOrder(Order o);
+        void AddProduct(Product p);
+        void AddProduct(string name, decimal price, int Amount);
+        void AddPerson(Person p);
+        public IEnumerable<Order> GetOrders();
+        public IEnumerable<Person> GetPersons();
+        public IEnumerable<Product> GetProducts();
+
+
+    }
+}
